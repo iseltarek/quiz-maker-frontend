1a4ee5697dc9e53a3fe46094fd38c6f2f34f0d70
isel tarek <iseltarek15@gmail.com>
2025-04-19 22:03:52 +0200

    separate teacher from home .

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { SharedMaterialModule } from '../../modules/shared-material.module';
+import { TeacherComponent } from '../../components/teacher/teacher.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [SharedMaterialModule],
+  imports: [SharedMaterialModule, TeacherComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private authService: AuthService) {}
   logout() {
-    throw new Error('Method not implemented.');
+    this.authService.logout();
   }
 }
